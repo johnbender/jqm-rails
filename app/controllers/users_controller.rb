9b42834110542a3d6641193ba042393a6a136d0c
John Bender <john.m.bender@gmail.com>
2012-04-05 15:15:13 -0700

    users list and basic show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,12 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
